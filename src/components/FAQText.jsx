c98f64eab657a2292b6b90fdd14ac4f5f330309a
rickdeakins <rickdeakinsjr@gmail.com>
2024-03-03 00:18:37 -0500

    FAQ Text imported via component file.

---
+++ src/components/FAQText.jsx
@@ -0,0 +1,34 @@
+const FAQText = () => {
+    return(
+    <div>
+      <h3>What are typical feeding concerns for which parents would seek out feeding therapy?</h3>
+        <ul>
+            <li>Poor latch at breast or bottle</li>
+            <li>Inefficiency with feeding (taking too long at the breast or bottle)</li>
+            <li>Coughing or gagging at breast or bottle</li>
+            <li>Concern for tongue tie</li>
+            <li>Gagging when transitioning to blended foods (purée/traditional baby food)</li>
+            <li>Gagging when transitioning to table foods (solids)</li>
+            <li>Guidance with baby-led weaning</li>
+            <li>Food refusal</li>
+            <li>Self feeding refusal (my child will not feed themself)</li>
+            <li>Poor transition to cups (my child will not accept a cup)</li>
+        </ul><br/>
+      <h3>Which areas does Whole Practice Feeding therapy offer in-home services?</h3>
+        <p>Within a 20 mile radius of Cleveland. <br/><br/>Farther visits will be considered upon request and may require additional mileage fee.</p><br/>   
+      <h3>Does Whole Practice Feeding Therapy accept insurance?</h3>
+        <ul>
+            <li>We do not accept insurance however we do accept HSA/FSA.</li>
+            <li>No insurance headaches mean savings are passed down to you!</li>
+            <li>No billing surprises</li>
+            <li>No limited visits</li>
+            <li>No coverage maximums</li>
+            <li>No waitlists</li>
+            <li>Lower costs compared to clinic visits for initial consultation and bundle visits.</li>
+        </ul>     
+    </div>
+    
+    )
+}
+
+export default FAQText;
