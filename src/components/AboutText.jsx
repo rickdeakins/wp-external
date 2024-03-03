af9cc00b645cf327b46dd428ab87fbe2de9fea94
rickdeakins <rickdeakinsjr@gmail.com>
2024-03-03 00:17:17 -0500

    Text on About page imported as a component via this file.

---
+++ src/components/AboutText.jsx
@@ -0,0 +1,13 @@
+const AboutText = () => {
+    return(
+        <p>
+        Maria Podolan is an Occupational Therapist and Certified Lactation Counselor. Maria has over 13 years of experience as a feeding therapist, working with children in variety of settings including expansive experience in the hospital and clinic settings. For the past 6 years, Maria has worked in collaboration with specialists including breastfeeding medicine to target feeding skills that impact successful breast-feeding. In addition to this, Maria is also passionate about targeting feeding skills with table foods as soon as baby is demonstrating readiness skills in order to be successful and result in less stressful mealtimes in early toddlerhood. <br/> <br/>
+        Maria is a mother to two boys, both of whom struggled with early feeding difficulty with breastfeeding. Navigating through oral ties, body tension as well as resources of lactation, breastfeeding medicine and release providers were key to success to continue breastfeeding beyond the first year. <br/> <br/>
+        Maria has training in the SOS (Sequential Oral Sensory) approach to feeding. Maria has been a Certified Lactation Counselor for 8 years and is certified in TBML, Therapeutic Breast Massage in Lactation.<br/> <br/>
+        Maria has training in tongue tie assessment using the ATLFF, Assessment Tool for Lingual Frenulum Function, via the Hazelbaker Institute.<br/> <br/>
+        Additional methods and modalities used: Myofascial release, Trigger-point release, Kinesiotaping, and Infant Massage.
+      </p>
+    )
+}
+
+export default AboutText;
