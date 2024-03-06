3a533d19b78545bad7633e83fb2e4cb213bad11d
rickdeakins <rickdeakinsjr@gmail.com>
2024-03-06 14:14:26 -0800

    Components imported into Contact page. Hardcoded data removed to clean up code for clear seperation of concerns.

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,26 +1,29 @@
+import ContactInfo from '../components/ContactInfo';
+import ContactForm from '../components/ContactForm';
+
+const headingStyle= {
+  color:'#009999', 
+  textAlign: 'center'
+}
+
+const containerStyle = {
+  paddingTop: '20%',
+  paddingBottom: '10%',
+}
+
+const bodyStyle = {
+  margin: "0 20%",
+  textAlign: "center",
+}
+
 export default function Contact() {
     return (
-      <div className="bg-light container-fluid" style={{ marginTop: '20%' }}>
-        {/* <img src="images/contact.svg" alt="greetings" width="50%" className="pb-3"/>  */}
-        <div className="container d-flex justify-content-start align-items-center pb-3">
-          <p className="col-lg-5 text-center mx-auto bg-light">
-          <h3><strong>Contact With Any Inquiries</strong></h3>
-          <form style={{ marginBottom: '20%' }}>
-          <div class="mb-3">
-            <label for="Full Name" class="form-label">Full Name</label>
-            <input type="password" class="form-control" id="fullNameInput"/>
-          </div>  
-          <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-          </div>
-          <div className="mb-3">
-              <label htmlFor="messageInput" className="form-label">Message</label>
-              <textarea className="form-control" id="messageInput" rows="5"></textarea>
-              </div>
-          <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
-        </p><br/> 
-        </div></div>
+      <div className="bg-light container-fluid" style={containerStyle}>
+        <div className="align-items-center" style={bodyStyle}>
+          <h2 class="heading" style={headingStyle}>Contact</h2>
+        <ContactInfo />  
+        <ContactForm />
+        </div>    
+      </div> 
     );
   }
