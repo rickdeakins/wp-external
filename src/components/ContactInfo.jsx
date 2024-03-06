fab7e2c917495c3601a4430f1aba2e2c8ad2b292
rickdeakins <rickdeakinsjr@gmail.com>
2024-03-06 14:11:29 -0800

    Contact info separated onto its own component.

---
+++ src/components/ContactInfo.jsx
@@ -0,0 +1,26 @@
+const headerStyle = {
+    color:'#009999' 
+}
+
+const textStyle = {
+    color:'#000000' 
+}
+
+const textBody= {
+    textAlign: 'left',
+    margin: '0 30%'
+}
+
+
+const ContactInfo = () => {
+    return(
+   <div class="text align-items-center" style={textBody}>
+        <h5 style={headerStyle}>Email:</h5>
+        <p style={textStyle}>wholepractice@wholepracticefeedingtherapy.com</p><br/>
+        <h5 style={headerStyle}>Phone:</h5>
+        <p style={textStyle}>+12162305832</p><br/>
+   </div>
+   )
+}
+
+export default ContactInfo;
