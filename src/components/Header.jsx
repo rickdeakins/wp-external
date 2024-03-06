a31fa7129dfcf1e663642a78ca973fbfce5553cf
rickdeakins <rickdeakinsjr@gmail.com>
2024-03-06 11:42:30 -0800

    Header component has Navbar imported so single import will display both components.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,25 @@
+import Nav from './Navbar';
+
+const headerStyle = {
+    backgroundColor: '#ffffff', 
+    position: 'absolute', 
+    top: 0, 
+    left: 0, 
+    right: 0
+}
+
+
+
 function Header(){
     return(
-        <div className="text-white py-1 fixed-top">
+        <div className="text-white py-1 fixed-top" style={headerStyle}>
             <div className="container">
-            <img src="public/logo.svg" alt="logo" style={{ alignItems: 'center', padding: '20px', maxWidth: '20%' }} /> 
+            <img src="public/logo.svg" alt="logo" style={{ alignItems: 'center', padding: '20px', maxWidth: '20%', color: '#000000' }} /> 
             </div>
+            <Nav />
         </div>
+        
     )
 }
+
+export default Header;
