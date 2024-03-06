a11606338d5d95a2945559aa6d88c2b4b13ed9d3
rickdeakins <rickdeakinsjr@gmail.com>
2024-03-06 11:56:44 -0800

    Reconfigued the body of the page to correct some structural issues.

--- src/pages/FAQ.jsx
+++ src/pages/FAQ.jsx
@@ -1,19 +1,39 @@
 import FAQText from '../components/FAQText';
 
+const headingStyle= {
+  color:'#009999', 
+  textAlign: 'left'
+}
+
+const bodyStyle = {
+ margin: '10%',
+}
 export default function FAQ() {
       return (
-        <div className="bg-light container-fluid align-items-center text-center" style={{ paddingTop: '170px' }}>
-          {/* Heading image */}
-        {/* <img src="images/portfolio.svg" alt="greetings" width="50%" className="pb-3"/>  */}
-        <div className="row">
-          <div className="w-50 mx-auto">
-            <div className="d-flex flex-column justify-content-center align-items-center mb-5">
-                {/* Content for the first flex box */}
-                <h3>FAQ</h3><br/>
-                <FAQText />
-              </div> 
-            </div>
-        </div>  
-            </div> 
-      );
-    }
+    //     <div className="bg-light container-fluid align-items-center text-center" style={{ marginTop: '10%' }}>
+    //     <div className="row" style={bodyStyle}>
+    //         <div className="d-flex flex-column justify-content-center align-items-center mb-5">
+    //             {/* Content for the first flex box */}
+    //             <h2 className="heading" style={headingStyle}>FAQ</h2><br/>
+    //             <FAQText />
+    //         </div>
+    //     </div>  
+    //         </div> 
+    //   );
+    // } 
+
+
+<div className="bg-light container-fluid d-flex justify-content-center align-items-center" style={{ marginTop: '15%' }}>
+  <div className="body" style={bodyStyle}>
+      <div className="row">
+        {/* Left column for text */}
+        <div className="col-lg-10 text-left">
+          <h2 class="heading" style={headingStyle}>FAQ</h2>
+          <FAQText />
+        </div>
+      </div>
+    </div>
+</div>  
+  
+  );
+}
