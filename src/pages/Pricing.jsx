99c968842b94ac18dccb727811e77911474b6c3f
rickdeakins <rickdeakinsjr@gmail.com>
2024-03-06 12:04:52 -0800

    Adjustment to style components for return code streamline

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -1,11 +1,20 @@
+const containerStyle = {
+  paddingTop: '20%'
+}
+
+const imageStyle = {
+  maxWidth: '25%', 
+  borderRadius: '10%', 
+  marginTop: '5%',
+  marginBottom: '10%'
+}
+
 export default function Pricing() {
     return (
-      <div className="bg-light container-fluid" style={{ paddingTop: '20%' }}>
-        {/* <img src="images/resume.svg" alt="greetings" width="50%" className="pb-3"/>  */}
-       {/* <h2>Pricing</h2><br/> */}
-       <div className="align-items-center">
-        <img src="images/pricing-component.svg" alt="pricing" style={{ maxWidth: '30%', borderRadius: '10%', marginTop: '5%',marginBottom: '20%' }} /> 
+      <div className="bg-light container-fluid" style={containerStyle}>
+        <div className="align-items-center">
+          <img src="images/pricing-component.svg" alt="pricing" style={imageStyle} /> 
         </div>    
-       </div>
+      </div>
        )
   }
