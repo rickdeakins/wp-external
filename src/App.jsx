1f42cf5e2dae8277e6e4f8807648f4bfcc215cfd
rickdeakins <rickdeakinsjr@gmail.com>
2024-03-06 14:33:09 -0800

    Removal of header and footer hard code and import of components initiated as replacement.

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
 // Bringing in the required import from 'react-router-dom'
 import { Outlet } from 'react-router-dom';
 import Nav from './components/Navbar';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 
 function App() {
   // The Outlet component will conditionally swap between the different pages according to the URL
   return (
     <div>
-      <header className="text-white py-1 fixed-top margin-right">
-        <div className="container">
-        <img src="public/logo.svg" alt="logo" style={{ alignItems: 'center', maxWidth: '15%', margin: '2%' }} /> 
-        </div>
-        <Nav />
+      <header className="text-white py-1 fixed-top">
+        <Header />
       </header>   
       <main>
         <Outlet />
       </main>
-      <div id="footer" className="py-4"><strong>© 2024 Whole Practice Feeding Therapy</strong><br/>
-          <p>Whole Person, Whole Family Dynamic, Whole Projection to Success</p>
-      </div>
+      <Footer />
     </div>
   );
 }
