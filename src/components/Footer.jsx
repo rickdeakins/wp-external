d55f4b310dd6ce53e000ba11bdc8e80e06be6a6a
rickdeakins <rickdeakinsjr@gmail.com>
2024-03-06 14:31:58 -0800

    Separation of footer into component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+const footerStyle = {
+    backgroundColor: '#ffffff', 
+    position: 'relative', 
+    marginTop: 'auto'
+}
+
+
+function Footer(){
+    return(
+
+<div id="footer" className="py-4" style={footerStyle}>
+    <strong>© 2024 Whole Practice Feeding Therapy</strong><br/>
+        <p>Whole Person, Whole Family Dynamic, Whole Projection to Success</p>
+</div>
+
+    )
+}
+
+export default Footer;
