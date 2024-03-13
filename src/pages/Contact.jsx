0d4602ad6f268670f34b293ac298153f734566dc
rickdeakins <rickdeakinsjr@gmail.com>
2024-03-12 22:15:33 -0400

    Removal of unnecessary elements from code.

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,28 +1,11 @@
 import ContactInfo from '../components/ContactInfo';
 import ContactForm from '../components/ContactForm';
 
-const headingStyle= {
-  textAlign: 'center',
-  color:'#495057', 
-
-}
-
-
-const bodyStyle = {
-  margin: "0 25%",
-  textAlign: "center",
-  border: '2px',
-  borderRadius: '3%',
-  backgroundColor: '#fffcf9',
-  boxShadow: '2px 2px 4px rgba(1, 1, 1, 0.5)',
-  padding: '10px' 
-}
-
 export default function Contact() {
     return (
       <div className="ContactContainer bg-light container-fluid">
         <div className="ContactBody align-items-center" >
-          <h2 class="heading" style={headingStyle}>Contact</h2>
+          <h2 class="headerStyle">Contact</h2>
         <ContactInfo />  
         <ContactForm />
         </div>    
