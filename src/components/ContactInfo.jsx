477f64a655fa30373624e819989e3b64f61e795a
rickdeakins <rickdeakinsjr@gmail.com>
2024-03-12 22:12:48 -0400

    Styling attribute  migration to CSS file. Adjustments for resoponsive output. Still pending on some screen sizes

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -1,24 +1,10 @@
-const headerStyle = {
-    color:'#495057', 
-}
-
-const textStyle = {
-    color:'#000000' 
-}
-
-const textBody= {
-    textAlign: 'left',
-    margin: '0 10%'
-}
-
-
 const ContactInfo = () => {
     return(
-   <div class="text align-items-center" style={textBody}>
-        <h5 style={headerStyle}>Email:</h5>
-        <p style={textStyle}>wholepractice@wholepracticefeedingtherapy.com</p><br/>
-        <h5 style={headerStyle}>Phone:</h5>
-        <p style={textStyle}>+12162305832</p><br/>
+   <div class="text align-items-center">
+        <h5 class="headerStyle">Email:</h5>
+        <p class="contactText">wholepractice@wholepracticefeedingtherapy.com</p><br/>
+        <h5 class="headerStyle">Phone:</h5>
+        <p class="contactText">+12162305832</p><br/>
    </div>
    )
 }
