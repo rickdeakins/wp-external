727b0137318686402564cb646bb54b1d0ffc7842
rickdeakins <rickdeakinsjr@gmail.com>
2024-03-12 22:14:28 -0400

    Renaming of className structure for streamlined reference in CSS file

--- src/components/HomeImages.jsx
+++ src/components/HomeImages.jsx
@@ -2,22 +2,23 @@ import { Link } from 'react-router-dom';
 
 function HomeImages() {
     return (
-        <div className="homepageImages align-items-center" style={{minWidth: '50%'}} >
-            <div className="main-image">
+        <div className="homepageImages align-items-center" >
+            <div className="imagesContainer">
                 <Link to="/About">
-                    <img src="./images/patient.jpeg" alt="Main Image" style={{ width: '40%', height: 'auto', borderRadius: '2%', boxShadow: '2px 2px 4px rgba(1, 1, 1, 0.5)' }} />
+                    <img src="./images/patient.jpeg" className="main-image" />
                 </Link>
             <div className="sub-image-container">
                 <Link to="/Pricing">      
-                <img className="sub-image" src="./images/vancake.jpeg" alt="Sub Image 1" style={{ width: '20%', height: 'auto', borderRadius: '2%', boxShadow: '2px 2px 4px rgba(1, 1, 1, 0.5)'}} />
+                <img className="sub-image" src="./images/vancake.jpeg" alt="Sub Image 1" />
                 </Link>
                 <Link to="/FAQ">
-                <img className="sub-image" src="./images/vancake2.jpeg" alt="Sub Image 2" style={{ width: '20%', height: 'auto', borderRadius: '2%', boxShadow: '2px 2px 4px rgba(1, 1, 1, 0.5)'}} />
+                <img className="sub-image" src="./images/vancake2.jpeg" alt="Sub Image 2"/>
                 </Link>
             </div></div>
         </div>  
     );
 }
+
 
 export default HomeImages;
 
