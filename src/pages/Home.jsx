6dec393b8e7d5f4528210a7af8bed809f4d09323
rickdeakins <rickdeakinsjr@gmail.com>
2024-03-14 22:17:48 -0400

    Correctng some minor issues, planed ahead for home page redesign

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,9 @@
 import HomeImages from '../components/HomeImages';
 
-const bodyStyling = {
-    paddingTop: '20%',
-    paddingBottom: '10%'
-}
-
-
-
 export default function Home() {
     return (
         <main>
-            <div className="mainBody bg-light container-fluid" style={bodyStyling}>
+            <div className="mainBody bg-light container-fluid" >
                 <img src ="./public/images/welcomeImage.svg" className='welcomeImage'/>
                 <HomeImages />
             </div>
