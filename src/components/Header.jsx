1d34cec6374cba97fac896b5891055c150b207c6
rickdeakins <rickdeakinsjr@gmail.com>
2024-03-14 22:14:14 -0400

    Moving around some data in prep of removal of all locally sourced styling.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,13 @@ const headerStyle = {
     backgroundColor: '#ffffff', 
     position: 'relative', 
     top: 0,
-    // left: 0, 
-    // right: 0
+    left: 0, 
+    right: 0
 }
 
 function Header(){
     return(
-        <div className="text-white py-1 fixed-top" style={headerStyle}>
+        <div className="headerstyle text-white py-1 fixed-top" >
             <div className="container">
             <img src="public/logo.svg" alt="logo" style={{ alignItems: 'center', padding: '10px', maxWidth: '15%', minWidth: '150px', color: '#000000' }} /> 
             </div>
