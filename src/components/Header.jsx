f4cbe1d7fb8d6dbcda7446f004e1fc352c939003
rickdeakins <rickdeakinsjr@gmail.com>
2024-03-18 21:51:43 -0400

    Removed styling from header file and updated css file with settings - logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,13 @@
 import Nav from './Navbar';
 
-const headerStyle = {
-    backgroundColor: '#ffffff', 
-    position: 'relative', 
-    top: 0,
-    left: 0, 
-    right: 0
-}
-
 function Header(){
     return(
         <div className="headerstyle text-white py-1 fixed-top" >
             <div className="container">
-            <img src="public/logo.svg" alt="logo" style={{ alignItems: 'center', padding: '10px', maxWidth: '15%', minWidth: '150px', color: '#000000' }} /> 
+                <img src="public/logo.svg" alt="logo" className="logoStyle"/> 
             </div>
             <Nav />
-        </div>
-        
+        </div>   
     )
 }
 
